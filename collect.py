b95cde939741e0ee22761c1f6f52f94fdf0def96
Juciano Cardoso <cjuciano@gmail.com>
2019-03-08 02:21:37 -0300

    Added picture capture class.

---
+++ collect.py
@@ -0,0 +1,32 @@
+import time
+from multiprocessing import Process
+from multiprocessing import Manager
+
+from loguru import logger
+
+from picture_capture import PicutureCapture
+
+
+class Collect:
+
+    def __init__(self):
+        self.picture_capture = PicutureCapture(1, 'captures')
+
+    def run(self):
+        with Manager() as manager:
+            picture_capture_collect_event = manager.Event()
+            picture_capture_process = Process(
+                target=self.picture_capture.capture,
+                args=(picture_capture_collect_event,))
+            picture_capture_process.start()
+            for i in range(5):
+                logger.info('Part {}', i)
+                picture_capture_collect_event.set()
+                time.sleep(10)
+                picture_capture_collect_event.clear()
+            picture_capture_process.terminate()
+            time.sleep(1)
+            logger.info("Process alive: {}", picture_capture_process.is_alive())
+
+if __name__ == '__main__':
+    Collect().run()
---
+++ picture_capture.py
@@ -0,0 +1,27 @@
+import os
+import time
+from datetime import datetime
+
+from loguru import logger
+import pytz
+import cv2
+
+
+class PicutureCapture:
+
+    def __init__(self, interval, output_path):
+        self.interval = interval
+        self.output_path = output_path
+        self.video_capture = cv2.VideoCapture(0)
+
+    def create_image_output_path(self, capture_datetime):
+        return os.path.join(self.output_path, "capture_{}.png".format(capture_datetime.isoformat()))
+
+    def capture(self, collect_event):
+        while True:
+            collect_event.wait()
+            current_datetime = pytz.utc.localize(datetime.utcnow())
+            output_image_name = self.create_image_output_path(current_datetime)
+            _, frame = self.video_capture.read()
+            cv2.imwrite(output_image_name, frame)
+            time.sleep(self.interval)
