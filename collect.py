b6023d7a8d4439d61ba3df099d5f6c094c86dc45
Juciano Cardoso <cjuciano@gmail.com>
2019-03-08 03:16:37 -0300

    Formatted files and improved code structure in picture capture.

--- collect.py
+++ collect.py
@@ -9,25 +9,19 @@ from picture_capture import PicutureCapture
 
 class Collect:
 
-    def __init__(self):
-        self.picture_capture = PicutureCapture(1, 'captures')
-
     def run(self):
         with Manager() as manager:
-            picture_capture_collect_event = manager.Event()
-            picture_capture_terminate_event = manager.Event()
-            picture_capture_process = Process(
-                target=self.picture_capture.capture,
-                args=(picture_capture_collect_event, picture_capture_terminate_event))
-            picture_capture_process.start()
-            for i in range(5):
-                logger.info('Part {}', i)
-                picture_capture_collect_event.set()
-                time.sleep(10)
-                picture_capture_collect_event.clear()
-            picture_capture_terminate_event.set()
-            picture_capture_process.join()
-            logger.info("Process alive: {}", picture_capture_process.is_alive())
+            with PicutureCapture(1, 'captures', manager.Event(),
+                                 manager.Event()) as picture_capture:
+                for i in range(5):
+                    logger.info('Part {}', i)
+                    picture_capture.start_capture()
+                    time.sleep(10)
+                    picture_capture.stop_capture()
+
+            logger.info("Process alive: {}",
+                        picture_capture.capture_process.is_alive())
+
 
 if __name__ == '__main__':
     Collect().run()
--- picture_capture.py
+++ picture_capture.py
@@ -1,6 +1,7 @@
 import os
 import time
 from datetime import datetime
+from multiprocessing import Process
 
 from loguru import logger
 import pytz
@@ -9,19 +10,63 @@ import cv2
 
 class PicutureCapture:
 
-    def __init__(self, interval, output_path):
+    def __init__(self,
+                 interval,
+                 output_path,
+                 capture_event=None,
+                 terminate_event=None):
         self.interval = interval
         self.output_path = output_path
         self.video_capture = cv2.VideoCapture(0)
+        self.capture_process = None
+        self._capture_event = capture_event
+        self._terminate_event = terminate_event
 
-    def create_image_output_path(self, capture_datetime):
-        return os.path.join(self.output_path, "capture_{}.png".format(capture_datetime.isoformat()))
+    @property
+    def capture_event(self):
+        return self._capture_event
 
-    def capture(self, collect_event, terminate_event):
+    @property
+    def terminate_event(self):
+        return self._terminate_event
+
+    @capture_event.setter
+    def capture_event(self, event):
+        self._capture_event = event
+
+    @terminate_event.setter
+    def terminate_event(self, event):
+        self._terminate_event = event
+
+    def __enter__(self):
+        assert self.capture_event and self.terminate_event, "Flow control events must be setted"
+        self.capture_process = Process(
+            target=self._capture_process,
+            args=(self.capture_event, self.terminate_event))
+        self.capture_process.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.terminate_event.set()
+        self.capture_process.join()
+
+    def start_capture(self):
+        assert self.capture_process, "Capture process not started."
+        self.capture_event.set()
+
+    def stop_capture(self):
+        assert self.capture_process, "Capture process not started."
+        self.capture_event.clear()
+
+    def _capture_and_save_image(self):
+        output_image_name = os.path.join(
+            self.output_path, "capture_{}.png".format(
+                pytz.utc.localize(datetime.utcnow()).isoformat()))
+        _, frame = self.video_capture.read()
+        cv2.imwrite(output_image_name, frame)
+
+    def _capture_process(self, capture_event, terminate_event):
         while not terminate_event.is_set():
-            collect_event.wait()
-            current_datetime = pytz.utc.localize(datetime.utcnow())
-            output_image_name = self.create_image_output_path(current_datetime)
-            _, frame = self.video_capture.read()
-            cv2.imwrite(output_image_name, frame)
+            capture_event.wait()
+            self._capture_and_save_image()
             time.sleep(self.interval)
